2fb6ecc68350385fc3076f342d919cecf6f14e12
Kristian Dalmo Olsen <kristian@dalmo.me>
2020-11-18 19:12:54 +0100

    Updated installer

--- install.ps1
+++ install.ps1
@@ -1,12 +1,16 @@
-# SCOOP
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+$DOTFILES_PATH = "$HOME/code/github.com/itsdalmo/dotfiles"
+$DOTFILES_REPO = "git@github.com:itsdalmo/dotfiles.git"
+
+Write-Host "Setting up scoop..."
+if (-Not ( Get-Command scoop -errorAction SilentlyContinue)) {
+    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+    scoop bucket add extras
+}
 
 scoop install git 
 scoop install ripgrep
 scoop install fzf
-
-scoop bucket add extras
 scoop install googlechrome
 scoop install vscode
 scoop install signal
@@ -14,10 +18,24 @@ scoop install discord
 scoop install steam
 scoop install obs-studio-small
 
-# CHOCO
-Set-ExecutionPolicy Bypass -Scope Process
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
-choco feature enable -n allowGlobalConfirmation
+Write-Host "Setting up chocolatey..."
+if (-Not ( Get-Command choco -errorAction SilentlyContinue)) {
+    Set-ExecutionPolicy Bypass -Scope Process
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
+    choco feature enable -n allowGlobalConfirmation
+}
 
 choco install spotify
-choco install steelseries-engine
+choco install steelseries-engine --limit-output
+choco install twitch
+choco install tukui
+
+Write-Host "Configuring dotfiles.."
+if (-Not ( Test-Path $DOTFILES_PATH -PathType Container)) {
+    git clone $DOTFILES_REPO $DOTFILES_PATH
+}
+
+$dotfiles = @(".gitconfig", ".vimrc", ".gnupg/gpg.conf", ".gnupg/gpg-agent.conf")
+foreach ($file in $dotfiles) {
+    New-Item -ItemType SymbolicLink -Path "$HOME/$file" -Target "$DOTFILES_PATH/files/$file" -Force
+}
